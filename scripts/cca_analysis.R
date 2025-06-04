3942726e357ac3961dd8f77a4a391082a37983bd
esuglia <esuglia@gmail.com>
2025-06-04 08:09:55 -0700

    Cleaned up CCA figure

--- scripts/cca_analysis.R
+++ scripts/cca_analysis.R
@@ -1,5 +1,6 @@
 library(mixOmics)
 library(tidyverse)
+library(ggrepel)
 
 # Load data ----
 morph = read_csv("data/qpc_morph_9.6.24.csv")
@@ -9,10 +10,10 @@ climate_summary = read_csv("data/climate_summary.csv")
 # climate metrics removed: tmin (correlated with tmx), pck (correlated w/ppt)
 
 # Prep data for analysis ----
-morph_climate = morph |>
-  dplyr::select(ID, pop, site, lf_thickness:postvern_no_lvs, PC1, PC2, sla) |>
+morph_climate = morph %>%
+  dplyr::select(ID, pop, site, lf_thickness:postvern_no_lvs, PC1, PC2, sla) %>%
   # join climate and morphological dataframes
-  left_join(climate_summary) |>
+  full_join(climate_summary) %>%
   # remove NA's (needed for cancor to run)
   na.exclude()
 
@@ -35,22 +36,31 @@ morph_for_cca_df = data.frame(morph_for_cca)
 climate_for_cca_df = data.frame(climate_for_cca)
 
 # Run CCA ----
-
-result.cca = rcc(morphology, climate) # run the CCA method
+result.cca = rcc(morph_for_cca_df, climate_for_cca_df) # run the CCA method
 
 # plot projection into canonical variate subspace
 plotIndiv(result.cca) 
 # plot original variables' correlation with canonical variates
 vardata = plotVar(result.cca)
 
+row.names(vardata)
+#c("lf_thickness", "postvern_stem_diam", "postvern_height", "postvern_lngst_lf", "postvern_no_lvs", "sla", "el", "cwd", "str", "tmx", "ppt", "season_length")
+
+vardata_for_plotting = vardata %>%
+  mutate(labels = c("leaf thickness", "stem diameter", "height", "length of longest leaf", "number of leaves", "sla", "el", "cwd", "str", "tmx", "ppt", "season length"))
+
 # Use the var data to make the custom plot
 # Change the colour pallete as needed
-ggplot(vardata, aes(x = x, y = y, colour = col, label = names))+
+ggplot(vardata_for_plotting, aes(x = x, y = y, colour = col, label = labels))+
 #geom_point(size = 2)+
 geom_segment(aes(x = 0, y = 0, xend = x, yend = y),
   arrow = arrow(length = unit(0.2, "cm")), linewidth = 0.7) +
-geom_text_repel(size = 6)+
+#geom_text_repel(size = 6)+
+  geom_label_repel(size = 5)+
 scale_colour_identity()+
-theme_doc()+
+theme_classic()+
 labs(x = "Canonical Variate 1", y = "Canonical Variate 2", title = "Canonical Correlation Analysis")
-ggsave("cca_biplot_mixomics.pdf", width = 8, height = 8)
+
+# save plot
+ggsave("figures/cca_biplot_mixomics.pdf", width = 8, height = 8)
+ggsave("figures/cca_biplot_mixomics.png", width = 8, height = 8)
